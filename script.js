5c8d3f237cea0d41b5bbfec5ebf05f8b0cc1970a
xoxin <xoxinweb@gmail.com>
2025-04-16 08:53:41 +0300

    Version 1

---
+++ script.js
@@ -0,0 +1,44 @@
+const slider = document.querySelector('.reviews__slider');
+const prevButton = document.querySelector('.reviews__nav--prev');
+const nextButton = document.querySelector('.reviews__nav--next');
+const cards = document.querySelectorAll('.reviews__card');
+let currentIndex = 0;
+
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % cards.length;
+    updateSlider();
+});
+
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + cards.length) % cards.length;
+    updateSlider();
+});
+
+function updateSlider() {
+    const cardWidth = cards[0].offsetWidth + 32;
+    slider.scrollTo({
+        left: currentIndex * cardWidth,
+        behavior: 'smooth'
+    });
+}
+
+document.addEventListener('mousemove', (e) => {
+    const cards = document.querySelectorAll('.reviews__card');
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    cards.forEach(card => {
+        const rect = card.getBoundingClientRect();
+        const cardX = rect.left + rect.width/2;
+        const cardY = rect.top + rect.height/2;
+        
+        const angleX = (mouseY - cardY) / 30;
+        const angleY = (mouseX - cardX) / -30;
+
+        card.style.transform = `perspective(1000px) rotateX(${angleX}deg) rotateY(${angleY}deg)`;
+    });
+});
+
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('.nav');
+});
